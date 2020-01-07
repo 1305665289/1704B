297f493ce526628900de800159b3c7612cf45da9
jasonandjay <342690199@qq.com>
2020-01-07 14:46:59 +0800

    增加css module和导航守卫

--- Project2/easy-market/src/router/index.tsx
+++ Project2/easy-market/src/router/index.tsx
@@ -3,8 +3,26 @@ import RouterView from './RouterView'
 import config from './routerConfig'
 import {BrowserRouter} from 'react-router-dom'
 
+import styles from './index.module.scss'
+import {Router} from 'react-router'
+import {createHashHistory} from 'history'
+
+const history = createHashHistory();
+let token = '';
+const beforeEach = (location?:any)=>{
+    // 根据cookie中是否有登录态，和去的页面是否是登陆页面判断
+    if (!token && location.pathname !== '/login'){
+        history.replace('/login');
+    }
+}
+const unListen = history.listen(beforeEach);
+beforeEach(history.location);
+
 export default ()=>{
-    return <BrowserRouter>
+    return <Router history={history}> 
+        <h1 className={styles.h1}>我是H1标题</h1>
+        <h1 className="h1">我是H1标题</h1>
+        <h1 id="h1" className={styles.h1}>我是H1标题</h1>
         <RouterView routes={config.routes}/>
-    </BrowserRouter>
+    </Router>
 }
--- Project2/easy-market/src/router/routerConfig.tsx
+++ Project2/easy-market/src/router/routerConfig.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import RouterView from './RouterView'
+import {Link} from 'react-router-dom'
 
 export default {
     routes: [{
         path: '/login',
-        component: ()=><p>登陆页面</p>
+        component: ()=>(<><p>登陆页面</p><Link to="/main/home">去主页面</Link></>)
     },{
         path: '/main',
         component: (props:any)=>(<>
             <p>主页面</p>
             <RouterView routes={props.routes}></RouterView>
+            <footer>
+                <Link to="/main/home">首页</Link>
+                <Link to="/main/classify">分类</Link>
+                <Link to="/main/cart">购物车</Link>
+                <Link to="/main/my">我的</Link>
+            </footer>
         </>),
         children: [{
             path: '/main/home',
